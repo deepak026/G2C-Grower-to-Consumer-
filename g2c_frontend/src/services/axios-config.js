9da387da347c49f63ef8d07109829e4cc9e7e268
deepak026 <73790848+deepak026@users.noreply.github.com>
2024-05-22 16:10:19 +0530

    Update axios-config.js

--- g2c_frontend/src/services/axios-config.js
+++ g2c_frontend/src/services/axios-config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:2000"; //base url is case sensitive don't change it
+//const baseURL = "http://localhost:2000"; //base url is case sensitive don't change it
 const baseURL = "https://g2c-grower-to-consumer.onrender.com"; //base url is case sensitive don't change it
 
 const publicAxios = axios.create({ baseURL });
